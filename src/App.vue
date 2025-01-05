bdeff1dd7a8f8bf29bb965a02bd0ac6f23fc6379
gabeFrancisco <gabegit2@gmail.com>
2025-01-05 20:20:45 -0300

    created types and composition api

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,51 @@
 <template>
-  <div class="app">Hello world! God bless you!</div>
+  <div class="app">
+    Hello world! God bless you!
+    <p>{{ jobs[3].title}}</p>
+    <button>Change!</button>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import { Job } from './types/Job';
 
 export default defineComponent({
   name: 'App',
-  components: {
-  }
+  components: {},
+  setup() {
+    // const state = reactive({
+    //   name: "Some data...",
+    //   age: 25
+    // })
+
+    // return {
+    //   ...toRefs(state)
+    // }
+
+    const jobs = ref<Job[]>([
+      {
+        title: "farm_worker", location: "some fancy ranch", salary: 3000, id: 1
+      },
+      {
+        title: "electrician", location: "some fancy electrical facility", salary: 4000, id: 7
+      },
+      {
+        title: "truck_driver", location: "some fancy truck", salary: 3500, id: 15
+      },
+      {
+        title: "programmer", location: "some fancy place with a computer", salary: 5000, id: 3
+      },
+    ])
+
+    return { jobs }
+  },
 });
 </script>
 
 <style>
-  .app{
-    width: 100vw;
-    text-align: center;
-  }
+.app {
+  width: 100vw;
+  text-align: center;
+}
 </style>
